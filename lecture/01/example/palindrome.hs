52e368cc40150d5bfb2ce86b914d795155971776
kydos <angelo@icorsaro.net>
2020-09-10 00:33:04 +0200

    added material for first lecture

---
+++ lecture/01/example/palindrome.hs
@@ -0,0 +1,7 @@
+palindrome x = (x == (reverse x))
+
+palindrome2 x = case length x of
+  0 -> True
+  n -> if ((head x) == (last x))
+        then palindrome2 (take (n-2) $ drop 1 x)
+        else False
